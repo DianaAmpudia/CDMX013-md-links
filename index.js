c83209d748b6a196671e91a1d8b0f1c30c844f94
DianaAmpudia <dianalauraampudia@gmail.com>
2022-11-04 11:50:42 -0600

    adds minor changes to console messages

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk');
+const { exit } = require('process');
 const mdLinks = require('./mdLinks.js')
 
 // creamos la interfaz que voy a utilizar para recibir y mostrar información al usuario
@@ -8,18 +9,23 @@ const readline = require('readline').createInterface({
 })
 
 const welcome = () => {
-    console.log(chalk.whiteBright.bgMagenta.bold('========================================================================='))
-    console.log(chalk.whiteBright.bgMagenta.bold('                     🔆 BIENVENID@ A MD LINKS 🔆                         '))
-    console.log(chalk.whiteBright.bgMagenta.bold('========================================================================='))
-    console.log(chalk.whiteBright.bgMagenta.bold('A continuación deberas colocar una ruta y una de las siguientes opciones '))
-    console.log(chalk.whiteBright.bgMagenta.bold('--validate: Valida los links del archivo md                              '))
-    console.log(chalk.whiteBright.bgMagenta.bold('--stats: Te regresa el total de links y el número de links únicos        '))
-    console.log(chalk.whiteBright.bgMagenta.bold('                                                                         '))
+    console.log(chalk.whiteBright.bgMagenta.bold('=========================================================================='))
+    console.log(chalk.whiteBright.bgMagenta.bold('                     🔆 BIENVENID@ A MD LINKS 🔆                          '))
+    console.log(chalk.whiteBright.bgMagenta.bold('=========================================================================='))
+    console.log(chalk.whiteBright.bgMagenta.bold(' A continuación deberas colocar una ruta y una de las siguientes opciones '))
+    console.log(chalk.whiteBright.bgMagenta.bold(' --validate: Valida los links del archivo md                              '))
+    console.log(chalk.whiteBright.bgMagenta.bold(' --stats: Te regresa el total de links y el número de links únicos        '))
+    console.log(chalk.whiteBright.bgMagenta.bold('                                                                          '))
 }
 
 welcome();
-readline.question(chalk.black.bgYellowBright.bold(`✨ Ingresa una ruta✨:`), (path) => {
-  readline.question(chalk.black.bgYellowBright.bold(`✨ Ingresa una opción✨:`), (option) => {
+readline.question(chalk.black.bgYellowBright.bold(`🌱Ingresa una ruta:`), (path) => {
+  readline.question(chalk.black.bgYellowBright.bold(`🌱Ingresa una opción (--validate OR --stats):`), (option) => {
+    if (option === '--stats' || option === '--validate'){
     mdLinks(path, option);
+    } else {
+      console.log(chalk.whiteBright.bgMagenta.bold('❌La opción que ingresaste no es válida❌'))
+      exit()
+    }
   })
 })
